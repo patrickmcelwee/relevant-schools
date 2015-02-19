4a38d8d70bf9c6778d8bddd7ef38dbaeebc2d011
Patrick McElwee <pmm21@duke.edu>
2015-02-18 23:21:32 -0500

    Initial commit

---
+++ ui/app/app.js
@@ -0,0 +1,31 @@
+
+angular.module('sample', [
+  'ngRoute', 'ngCkeditor', 'sample.user', 'sample.search', 'sample.common', 'sample.detail',
+  'ui.bootstrap', 'gd.ui.jsonexplorer', 'sample.create'
+])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    'use strict';
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: '/search/search.html'
+      })
+      .when('/create', {
+        templateUrl: '/create/create.html',
+        controller: 'CreateCtrl'
+      })
+      .when('/detail', {
+        templateUrl: '/detail/detail.html',
+        controller: 'DetailCtrl'
+      })
+      .when('/profile', {
+        templateUrl: '/user/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
